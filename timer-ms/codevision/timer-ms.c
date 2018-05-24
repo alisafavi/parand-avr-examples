6b78f94ade186d77a15777bf428cd0fb62976dbf
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-05-24 18:00:24 +0430

    added timer-ms

---
+++ timer-ms/codevision/timer-ms.c
@@ -0,0 +1,83 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+© Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 24/05/2018
+Author  : 
+Company : 
+Comments: 
+
+
+Chip type               : ATmega16
+Program type            : Application
+AVR Core Clock frequency: 1/000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 256
+*******************************************************/
+
+#include <mega16.h>
+#include <alcd.h>
+#include <stdio.h>
+
+int ms = 0 , sec = 0, min = 0 , hour = 0;
+char st[20];
+
+interrupt [TIM0_OVF] void timer0_ovf_isr(void)
+{
+
+      ms++;
+      if(ms == 1000) {
+        sec++;
+        ms = 0;
+        if(sec == 60)
+        {
+            min++; 
+            sec=0;
+            if(min == 60)
+            {
+                hour++;
+                min = 0;
+                if(hour == 24) { hour = 0; }
+            }
+        }
+     
+      }
+
+}
+
+void main(void)
+{
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: 125/000 kHz
+// Mode: CTC top=OCR0
+// OC0 output: Toggle on compare match
+// Timer Period: 1 ms
+// Output Pulse(s):
+// OC0 Period: 2 ms Width: 1 ms
+TCCR0=(0<<WGM00) | (0<<COM01) | (1<<COM00) | (1<<WGM01) | (0<<CS02) | (1<<CS01) | (0<<CS00);
+TCNT0=0x00;
+OCR0=0x7C;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (1<<TOIE0);
+
+lcd_init(16);
+
+// Global enable interrupts
+#asm("sei")
+
+while (1)
+      {
+                sprintf(st, "%02d:%02d:%02d:%03d",hour,min, sec, ms);
+                lcd_gotoxy(2,0);
+                lcd_puts(st); 
+      }
+}
